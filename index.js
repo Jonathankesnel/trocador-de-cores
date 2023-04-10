0cfee56fac28087c1a7e8382df35278528ff2061
Jonathan Kesnel <112701895+Jonathankesnel@users.noreply.github.com>
2023-04-09 21:00:57 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,145 @@
+/* const colors = ["green", "red", "rgba(133,122,200)", "#f15025"]
+
+const btn =  document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', function() {
+ const randomNumber = getRandomNumber()
+ console.log(randomNumber)
+
+ document.body.style.backgroundColor = colors[randomNumber]
+ color.textContent = colors[randomNumber]
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length)
+}
+*/
+
+/*
+const colors = ['black', 'blue', 'orange', 'red']
+
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', function() {
+    const randomNumber = getRandomNumber()
+    console.log(getRandomNumber())
+
+    document.body.style.backgroundColor = colors[randomNumber]
+    color.textContent = colors[randomNumber]
+
+
+})
+
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length)
+} */
+
+/*
+const colors = ['black', 'pink', 'red', 'blue', 'green', 'purble']
+
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', function() {
+    const randomNumber = getRandomNumber()
+    console.log(randomNumber)
+
+    document.body.style.backgroundColor = colors[randomNumber]
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length)
+}
+
+
+const colors = [ 'white', 'black', 'red', 'grow', 'gray', 'blue']
+
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', function() {
+    const randomNumber = getRandomNumber()
+    
+    document.body.style.backgroundColor = colors[randomNumber]
+} )
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length)
+} */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const colors = ['blue', 'red', 'black', 'green', 'yellow']
+
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', function() {
+    const randomNumber = getRandomNumber()
+    console.log(randomNumber)
+
+    document.body.style.backgroundColor = colors[randomNumber]
+
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length)
+}
+
+
+
+
+
